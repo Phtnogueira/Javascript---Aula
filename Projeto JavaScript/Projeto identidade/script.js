c862fec5f5946a4f8fab8f348b0710a657baa7a6
Paulo Henrique Torrezani Nogueira <phv1x@hotmail.com>
2022-04-27 16:30:01 -0300

    Updated

--- Projeto JavaScript/Projeto identidade/script.js
+++ Projeto JavaScript/Projeto identidade/script.js
@@ -2,8 +2,10 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.getElementById('res')
-    if (fano.ariaValueMax.length == 0 || fano.value > ano) {
+    var res = document.querySelector('div#res')
+    if (fano.Value.length == 0 || fano.value > ano) {
         window.alert('[ERRO]Verifique os dados e tente novamente!')
+    } else {
+        window.alert ('tudo ok!')
     }
 }
