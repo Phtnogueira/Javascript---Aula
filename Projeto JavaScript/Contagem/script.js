e8214b62bce19042e853816fe15dc54e58506f1d
Paulo Henrique Torrezani Nogueira <phv1x@hotmail.com>
2022-05-09 11:48:43 -0300

    Update

--- Projeto JavaScript/Contagem/script.js
+++ Projeto JavaScript/Contagem/script.js
@@ -5,12 +5,17 @@ function contar() {
     let res = document.getElementById('res')
 
    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-      window.alert('[ERRO] Faltam dados')
+       res.innerHTML = 'Impossivel contar!'
+       //window.alert('[ERRO] Faltam dados')
     } else {
         res.innerHTML = 'Contando: '
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
+        if (p <= 0){
+            window.alert('Passso inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f){
             // Contagem Crescente
             for(let c = i; c <= f; c += p) {
