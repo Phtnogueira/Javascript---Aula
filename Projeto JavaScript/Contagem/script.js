23e7b3292a4d4a7c4d2f6d9d1a3f56e01bc937aa
Paulo Henrique Torrezani Nogueira <phv1x@hotmail.com>
2022-05-11 18:43:05 -0300

    Updated

--- Projeto JavaScript/Contagem/script.js
+++ Projeto JavaScript/Contagem/script.js
@@ -24,7 +24,7 @@ function contar() {
         }else {
             // Contagem Regressiva
             for (let c = i; c <= f; c -= p) {
-                res.innerHTML = ` ${c} \u{1F449}`
+                res.innerHTML = `${c} \u{1F449}`
             }
         }
         res.innerHTML += `\u{1F3C1} `
