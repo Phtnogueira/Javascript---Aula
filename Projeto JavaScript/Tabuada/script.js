bcd8f0ccfd207f6b837ad730b9e915309a66bfa8
Paulo Henrique Torrezani Nogueira <phv1x@hotmail.com>
2022-05-17 11:46:22 -0300

    Updated

--- Projeto JavaScript/Tabuada/script.js
+++ Projeto JavaScript/Tabuada/script.js
@@ -1,4 +1,10 @@
 function tabuada() {
     let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
+    if (num.value.length == 0 ){
+        let n = Number(num.value)
+    }else {
+        window.alert('Por favor, digite um número!')
+    }
+    
 }
