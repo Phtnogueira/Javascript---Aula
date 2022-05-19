d251276bb7695cbacd832371cb6a81ff24106832
Paulo Henrique Torrezani Nogueira <phv1x@hotmail.com>
2022-05-19 11:44:09 -0300

    Updated

--- Projeto JavaScript/Exercicio/vetornatela.js
+++ Projeto JavaScript/Exercicio/vetornatela.js
@@ -10,6 +10,6 @@ console.log(valores[4])
 console.log(valores[5])
 */
 
-for(let pos=0; pos < valores.length; pos++) {
+for(let pos = 0; pos < valores.length; pos++){
     console.log(`A posição ${pos} tem o valor ${valores[pos]}`)
 }
